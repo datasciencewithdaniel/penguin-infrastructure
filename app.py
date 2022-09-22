f752c05a0295c38f0544f2a732ba57764671b110
Daniel Chegwidden <daniel.chegwidden.datascience@gmail.com>
2022-09-22 17:41:41 +0800

    fix destroy

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ PenguinInfrastructureStack(
     # env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
-    env=cdk.Environment(account="162455609113", region="ap-southeast-2"),
+    # env=cdk.Environment(account="162455609113", region="ap-southeast-2"),
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
 )
 
