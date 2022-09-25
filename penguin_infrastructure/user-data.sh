1c2b9d7a21c1e96d1c46f00cee5bf847772b22cf
Daniel Chegwidden <daniel.chegwidden.datascience@gmail.com>
2022-09-25 14:51:23 +0800

    edit user-data

--- penguin_infrastructure/user-data.sh
+++ penguin_infrastructure/user-data.sh
@@ -3,6 +3,4 @@
 sudo yum update -y
 sudo yum install git -y
 
-sudo touch ~/test.txt
-
-sudo git clone https://github.com/datasciencewithdaniel/penguin.git
+sudo git clone https://github.com/datasciencewithdaniel/penguin.git ~/penguin
