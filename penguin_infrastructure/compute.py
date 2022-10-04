ddafcf967b0b3ba69656e91e47f0ea9480d2fd7a
Daniel Chegwidden <daniel.chegwidden.datascience@gmail.com>
2022-10-04 10:39:49 +0800

    edit params and user-data

--- penguin_infrastructure/compute.py
+++ penguin_infrastructure/compute.py
@@ -9,15 +9,11 @@ def create_user_data(self):
     self.user_data.add_commands("apt install make -y")
     self.user_data.add_commands("apt install python3-pip -y")
     # self.user_data.add_commands(f"export AWS_DEFAULT_REGION={self.AWS_DEFAULT_REGION}")
-    # self.user_data.add_commands(f"mkdir ~/.aws && touch ~/.aws/config")
-    # self.user_data.add_commands(
-    #     f"echo '[default]\nregion = {self.AWS_DEFAULT_REGION}' > ~/.aws/config"
-    # )
     self.user_data.add_commands(
         "git clone -b develop --single-branch https://github.com/datasciencewithdaniel/penguin.git"
     )
     self.user_data.add_commands(
-        f"cd penguin && sudo python3 -m pip install -r requirements.txt && sudo python3 -m bot.penguin --bot 1 --discord {self.DISCORD_TOKEN} --guild '{self.GUILD_NAME}'"
+        f"cd penguin && sudo python3 -m pip install -r requirements.txt && sudo python3 -m bot.penguin --bot 1 --discord {self.DISCORD_TOKEN} --guild {self.GUILD_NAME} --acount {self.AWS_ACCOUNT_DSWD}"
     )
 
 
@@ -25,9 +21,7 @@ def create_instance(self):
     instance_type = aws_ec2.InstanceType(config.INSTANCE_TYPE)
     ami_image = aws_ec2.MachineImage().generic_linux(
         {"ap-southeast-2": "ami-09a5c873bc79530d9"}
-    )  # .latest_amazon_linux()
-    # with open("./penguin_infrastructure/user-data.sh") as file:
-    #     user_data = file.read()
+    )
 
     self.instance = aws_ec2.Instance(
         self,
