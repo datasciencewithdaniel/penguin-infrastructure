ab3cbab5683ac86131cd7653a31cdb7ec62c3331
Daniel Chegwidden <daniel.chegwidden.datascience@gmail.com>
2022-12-05 22:07:24 +0800

    fix missing token

--- penguin_infrastructure/compute.py
+++ penguin_infrastructure/compute.py
@@ -2,33 +2,33 @@ from aws_cdk import aws_ec2
 from . import config
 
 
-def create_user_data(self):
-    self.user_data = aws_ec2.UserData.for_linux()
-    self.user_data.add_commands("apt-get update -y")
-    self.user_data.add_commands("apt-get upgrade -y")
-    self.user_data.add_commands("apt install make -y")
-    self.user_data.add_commands("apt install python3-pip -y")
-    self.user_data.add_commands(
-        "apt install -y ca-certificates curl gnupg lsb-release -y"
-    )
-    self.user_data.add_commands(
-        "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg"
-    )
-    self.user_data.add_commands(
-        'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null'
-    )
-    self.user_data.add_commands("apt-get update -y")
-    self.user_data.add_commands("apt install docker-ce docker-ce-cli containerd.io -y")
-    self.user_data.add_commands(
-        'curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose'
-    )
-    self.user_data.add_commands("chmod +x /usr/local/bin/docker-compose")
-    self.user_data.add_commands(
-        "git clone -b main --single-branch https://github.com/datasciencewithdaniel/penguin.git /home/penguin"
-    )
-    self.user_data.add_commands(
-        f'cd /home/penguin && make {self.COMMAND} DISCORD_TOKEN={self.DISCORD_TOKEN} GUILD_NAME="{self.GUILD_NAME}" AWS_ACCOUNT_DSWD={self.AWS_ACCOUNT_DSWD}'
-    )
+# def create_user_data(self):
+#     self.user_data = aws_ec2.UserData.for_linux()
+#     self.user_data.add_commands("apt-get update -y")
+#     self.user_data.add_commands("apt-get upgrade -y")
+#     self.user_data.add_commands("apt install make -y")
+#     self.user_data.add_commands("apt install python3-pip -y")
+#     self.user_data.add_commands(
+#         "apt install -y ca-certificates curl gnupg lsb-release -y"
+#     )
+#     self.user_data.add_commands(
+#         "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg"
+#     )
+#     self.user_data.add_commands(
+#         'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null'
+#     )
+#     self.user_data.add_commands("apt-get update -y")
+#     self.user_data.add_commands("apt install docker-ce docker-ce-cli containerd.io -y")
+#     self.user_data.add_commands(
+#         'curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose'
+#     )
+#     self.user_data.add_commands("chmod +x /usr/local/bin/docker-compose")
+#     self.user_data.add_commands(
+#         "git clone -b main --single-branch https://github.com/datasciencewithdaniel/penguin.git /home/penguin"
+#     )
+#     self.user_data.add_commands(
+#         f'cd /home/penguin && make {self.COMMAND} DISCORD_TOKEN={self.DISCORD_TOKEN} GUILD_NAME="{self.GUILD_NAME}" AWS_ACCOUNT_DSWD={self.AWS_ACCOUNT_DSWD}'
+#     )
 
 
 def create_instance(self):
